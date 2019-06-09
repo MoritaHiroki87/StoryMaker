bb30bc310f9085d1a51af1ebfd9552e3da96b316
森田寛樹 <>
2019-06-09 15:24:28 +0900

    update: キリのいいところまでで一旦止めて、ブログ用のキャプチャをとる。

--- board/urls.py
+++ board/urls.py
@@ -5,13 +5,13 @@ from .views import *
 app_name = 'board'
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
-    path('<int:project_id>/', ProjectBoardView.as_view(), name='project_board'),
-    path('<int:project_id>/new-curtain/', CreateCurtainView.as_view(), name='create_curtain'),
-    path('<int:project_id>/<int:curtain_id>/', EditCurtainView.as_view(), name='edit_curtain'),
-    path('<int:project_id>/<int:curtain_id>/delete/', DeleteCurtainView.as_view(), name='delete_curtain'),
-    path('<int:project_id>/<int:curtain_id>/new-card/', CreateCardView.as_view(), name='create_card'),
-    path('<int:project_id>/<int:curtain_id>/<int:card_id>/', EditCardView.as_view(), name='edit_card'),
-    path('<int:project_id>/<int:curtain_id>/<int:card_id>/delete/', DeleteCardView.as_view(), name='delete_card'),
+    path('<str:project_id>/', ProjectBoardView.as_view(), name='project_board'),
+    path('<str:project_id>/new-curtain/', CreateCurtainView.as_view(), name='create_curtain'),
+    path('<str:project_id>/<str:curtain_id>/', EditCurtainView.as_view(), name='edit_curtain'),
+    path('<str:project_id>/<str:curtain_id>/delete/', DeleteCurtainView.as_view(), name='delete_curtain'),
+    path('<str:project_id>/<str:curtain_id>/new-card/', CreateCardView.as_view(), name='create_card'),
+    path('<str:project_id>/<str:curtain_id>/<str:card_id>/', EditCardView.as_view(), name='edit_card'),
+    path('<str:project_id>/<str:curtain_id>/<str:card_id>/delete/', DeleteCardView.as_view(), name='delete_card'),
     # pdfの練習
     path('easypdf/', HelloPDFView.as_view(), name='easypdf'),
 ]
