7866ebe3b5f49659c79978e89a14652b63c97876
森田寛樹 <>
2019-06-10 11:12:25 +0900

    fix: pathのパスコーダを修正

--- board/urls.py
+++ board/urls.py
@@ -5,13 +5,13 @@ from .views import *
 app_name = 'board'
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
-    path('<str:project_id>/', ProjectBoardView.as_view(), name='project_board'),
-    path('<str:project_id>/new-curtain/', CreateCurtainView.as_view(), name='create_curtain'),
-    path('<str:project_id>/<str:curtain_id>/', EditCurtainView.as_view(), name='edit_curtain'),
-    path('<str:project_id>/<str:curtain_id>/delete/', DeleteCurtainView.as_view(), name='delete_curtain'),
-    path('<str:project_id>/<str:curtain_id>/new-card/', CreateCardView.as_view(), name='create_card'),
-    path('<str:project_id>/<str:curtain_id>/<str:card_id>/', EditCardView.as_view(), name='edit_card'),
-    path('<str:project_id>/<str:curtain_id>/<str:card_id>/delete/', DeleteCardView.as_view(), name='delete_card'),
+    path('<uuid:project_id>/', ProjectBoardView.as_view(), name='project_board'),
+    path('<uuid:project_id>/new-curtain/', CreateCurtainView.as_view(), name='create_curtain'),
+    path('<uuid:project_id>/<uuid:curtain_id>/', EditCurtainView.as_view(), name='edit_curtain'),
+    path('<uuid:project_id>/<uuid:curtain_id>/delete/', DeleteCurtainView.as_view(), name='delete_curtain'),
+    path('<uuid:project_id>/<uuid:curtain_id>/new-card/', CreateCardView.as_view(), name='create_card'),
+    path('<uuid:project_id>/<uuid:curtain_id>/<uuid:card_id>/', EditCardView.as_view(), name='edit_card'),
+    path('<uuid:project_id>/<uuid:curtain_id>/<uuid:card_id>/delete/', DeleteCardView.as_view(), name='delete_card'),
     # pdfの練習
     path('easypdf/', HelloPDFView.as_view(), name='easypdf'),
 ]
